34ddbc7c917675efbedd7cb1090aae014e1ef033
AyushSmith00 <140578065+AyushSmith00@users.noreply.github.com>
2026-01-19 21:34:52 +0530

    created tokens and updated userSchema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -20,6 +20,10 @@ const userSchema = new mongoose.Schema(
             minlength: 6,
             select: false,
         },
+
+        refreshToken: {
+            type: String,
+        },
     },
     {timestamps: true}
 );
